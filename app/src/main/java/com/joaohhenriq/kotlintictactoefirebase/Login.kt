5f7ab8f5fba03011604497eba060eb391b02e8a9
joaohhenriq <joao_camargo10@hotmail.com>
2021-03-29 20:33:57 -0300

    Login with firebase

---
+++ app/src/main/java/com/joaohhenriq/kotlintictactoefirebase/Login.kt
@@ -0,0 +1,54 @@
+package com.joaohhenriq.kotlintictactoefirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+
+class Login : AppCompatActivity() {
+
+    private var mAuth : FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadMain()
+    }
+
+    fun btnLoginEvent(view: View){
+        loginToFirebase(edtEmail.text.toString(), edtPassword.text.toString())
+    }
+
+    private fun loginToFirebase(email: String, password: String) {
+        mAuth!!.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if(it.isSuccessful) {
+                    Toast.makeText(applicationContext, "Successfuly loged", Toast.LENGTH_LONG).show()
+                    loadMain()
+                } else {
+                    Toast.makeText(applicationContext, "Failed to login", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun loadMain() {
+        val currentUser = mAuth!!.currentUser
+
+        if(currentUser != null) {
+            var intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("email", currentUser.email)
+            intent.putExtra("uid", currentUser.uid)
+
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/joaohhenriq/kotlintictactoefirebase/MainActivity.kt
+++ app/src/main/java/com/joaohhenriq/kotlintictactoefirebase/MainActivity.kt
@@ -2,10 +2,202 @@ package com.joaohhenriq.kotlintictactoefirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
+
+    var activePlayer = 1
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+
+    var player1WinsCounts = 0
+    var player2WinsCounts = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+    }
+
+    fun btnClick(view: View) {
+
+        val btnSelected = view as Button
+
+        var cellId = 0
+        when (btnSelected.id) {
+            R.id.btn1 -> cellId = 1
+            R.id.btn2 -> cellId = 2
+            R.id.btn3 -> cellId = 3
+            R.id.btn4 -> cellId = 4
+            R.id.btn5 -> cellId = 5
+            R.id.btn6 -> cellId = 6
+            R.id.btn7 -> cellId = 7
+            R.id.btn8 -> cellId = 8
+            R.id.btn9 -> cellId = 9
+        }
+
+        playGame(cellId, btnSelected)
+    }
+
+    fun playGame(cellId: Int, btnSelected: Button) {
+        if (activePlayer == 1) {
+            btnSelected.text = "X"
+            btnSelected.setBackgroundResource(R.color.red)
+            player1.add(cellId)
+            activePlayer = 2
+            autoplay()
+        } else {
+            btnSelected.text = "O"
+            btnSelected.setBackgroundResource(R.color.yellow)
+            player2.add(cellId)
+            activePlayer = 1
+        }
+
+        btnSelected.isEnabled = false
+
+        checkWinner()
+    }
+
+    fun checkWinner() {
+        var winner = -1
+
+        //rows
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+            winner = 1
+        }
+        if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+            winner = 2
+        }
+
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+            winner = 1
+        }
+        if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+            winner = 2
+        }
+
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+            winner = 1
+        }
+        if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+            winner = 2
+        }
+
+        //columns
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+            winner = 1
+        }
+        if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+            winner = 2
+        }
+
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+            winner = 1
+        }
+        if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+            winner = 2
+        }
+
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+            winner = 1
+        }
+        if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+            winner = 2
+        }
+
+        //diagonal
+        if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
+            winner = 1
+        }
+        if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
+            winner = 2
+        }
+
+        if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
+            winner = 1
+        }
+        if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
+            winner = 2
+        }
+
+        //winner
+        if (winner == 1) {
+            player1WinsCounts++
+            Toast.makeText(this, "player one won the game", Toast.LENGTH_LONG).show()
+            restartGame()
+        } else if (winner == 2) {
+            player2WinsCounts++
+            Toast.makeText(this, "player two won the game", Toast.LENGTH_LONG).show()
+            restartGame()
+        }
+    }
+
+    fun autoplay() {
+        var emptyCells = ArrayList<Int>()
+
+        for (cellId in 1..9) {
+            if (!player1.contains(cellId) || !player2.contains(cellId)) {
+                emptyCells.add(cellId)
+            }
+        }
+
+        if(emptyCells.size == 0) {
+            restartGame()
+        }
+
+        var r = Random()
+        val randomIndex = r.nextInt(emptyCells.size)
+        val cellId = emptyCells[randomIndex]
+
+        var btnSelected:Button
+        btnSelected = when(cellId) {
+            1 -> btn1
+            2 -> btn2
+            3 -> btn3
+            4 -> btn4
+            5 -> btn5
+            6 -> btn6
+            7 -> btn7
+            8 -> btn8
+            9 -> btn9
+            else -> {btn1}
+        }
+
+        playGame(cellId, btnSelected)
+    }
+
+    fun restartGame(){
+        activePlayer = 1
+        player1.clear()
+        player2.clear()
+
+        for (i in 1..9) {
+            var btnSelected:Button? = when(i) {
+                1 -> btn1
+                2 -> btn2
+                3 -> btn3
+                4 -> btn4
+                5 -> btn5
+                6 -> btn6
+                7 -> btn7
+                8 -> btn8
+                9 -> btn9
+                else -> {btn1}
+            }
+
+            btnSelected!!.text = ""
+            btnSelected!!.setBackgroundResource(R.color.buttonColor)
+            btnSelected.isEnabled = true
+        }
+
+        Toast.makeText(this, "Player1: $player1WinsCounts, Player2: $player2WinsCounts", Toast.LENGTH_LONG).show()
     }
 }
